076524e0bcd199ebaef7e2eef22e50a7a8c69684
Michael Hale <49446855+mjhale22@users.noreply.github.com>
2019-04-15 12:52:32 -0400

    Update and rename VibrationSens2.py to VibrationSens.py

--- Inputs/VibrationSens.py
+++ Inputs/VibrationSens.py
@@ -2,8 +2,9 @@ from gpiozero import MotionSensor, LED
 from signal import pause
 
 vib = MotionSensor(4)
+led = LED(16)
 
-if vib = vib.when_motion:
-  print("Motion Detected")
+vib.when_motion: led.on
+vib.when_no_motion: led.off
 
 pause()
