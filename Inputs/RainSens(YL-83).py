38c089938af81f4c89ef06220d2395e801de6b50
hevans11 <49651038+hevans11@users.noreply.github.com>
2019-04-15 13:38:03 -0400

    Update RainSens(YL-83).py

--- Inputs/RainSens(YL-83).py
+++ Inputs/RainSens(YL-83).py
@@ -1,31 +1,14 @@
-int rainPin = A0;
-int greenLED = 6;
-int redLED = 7;
-// you can adjust the threshold value
-int thresholdValue = 500;
+#!/usr/bin/env python
 
-void setup(){
-  pinMode(rainPin, INPUT);
-  pinMode(greenLED, OUTPUT);
-  pinMode(redLED, OUTPUT);
-  digitalWrite(greenLED, LOW);
-  digitalWrite(redLED, LOW);
-  Serial.begin(9600);
-}
+from time import sleep
+import os
+import RPi.GPIO as GPIO
 
-void loop() {
-  // read the input on analog pin 0:
-  int sensorValue = analogRead(rainPin);
-  Serial.print(sensorValue);
-  if(sensorValue < thresholdValue){
-    Serial.println(" - It's wet");
-    digitalWrite(greenLED, LOW);
-    digitalWrite(redLED, HIGH);
-  }
-  else {
-    Serial.println(" - It's dry");
-    digitalWrite(greenLED, HIGH);
-    digitalWrite(redLED, LOW);
-  }
-  delay(500);
-}
+GPIO.setmode(GPIO.BCM)
+GPIO.setup(23, GPIO.IN)
+state = GPIO.input(23)
+
+if (state == 0):
+    print "Water detected!"
+else:
+    print "Water not detected"
