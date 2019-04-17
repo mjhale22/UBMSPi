42122651b3adc1dbb8b3f3af33d69d4870de3ac9
Michael Hale <49446855+mjhale22@users.noreply.github.com>
2019-04-17 14:32:50 -0400

    Update TempHumidSens(DHT11).py

--- Inputs/TempHumidSens(DHT11).py
+++ Inputs/TempHumidSens(DHT11).py
@@ -6,13 +6,9 @@ sensor=Adafruit_DHT.DHT11
  
 # Set GPIO sensor is connected to
 gpio=21
+
+humidity, temperature = Adafruit_DHT.read(sensor, gpio)
  
-# Use read_retry method. This will retry up to 15 times to
-# get a sensor reading (waiting 2 seconds between each retry).
-humidity, temperature = Adafruit_DHT.read_retry(sensor, gpio)
- 
-# Reading the DHT11 is very sensitive to timings and occasionally
-# the Pi might fail to get a valid reading. So check if readings are valid.
 if humidity is not None and temperature is not None:
   print('Temp={0:0.1f}*C  Humidity={1:0.1f}%'.format(temperature, humidity))
 else:
