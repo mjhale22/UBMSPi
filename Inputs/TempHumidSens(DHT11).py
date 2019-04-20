b44189c820391c7d0b8ee5d1a92426a6ee4a2144
Michael Hale <49446855+mjhale22@users.noreply.github.com>
2019-04-19 23:44:14 -0400

    Update TempHumidSens(DHT11).py

--- Inputs/TempHumidSens(DHT11).py
+++ Inputs/TempHumidSens(DHT11).py
@@ -4,19 +4,21 @@
 #   cd Adafruit_Python_DHT
 #   sudo apt-get update
 
-#Import Adafruit_DHT Sensor Library to use script associated with sensor
+#Import Adafruit_DHT Sensor Library to use code associated with sensor
 import Adafruit_DHT
 
 # Set variable of sensor to (sensor being used) DHT11 : Options are DHT11, DHT22 or AM2302
 sensor=Adafruit_DHT.DHT11
  
-# Set variable of gpio as (pin) 21
+# Set variable of gpio as pin 21
 gpio=21
 
-#Set temperatureF to convert temperature (celsius) to fahrenheit
-temperatureF=(temperature*(9/5)+32)
-
+#Identify variables being pulled from Adafruit_DHT library (import Adafruit_DHT)
 humidity, temperature = Adafruit_DHT.read(sensor, gpio)
+
+#Set temperatureF to convert temperature (celsius) to fahrenheit
+#Previous script (line 17) identifying "temperature" from library must be listed above this line of code
+temperatureF=(temperature*(9/5)+32)
  
 if humidity is not None and temperature is not None:
   print('Temp={0:0.1f}*F  Humidity={1:0.1f}%'.format(temperatureF, humidity))
