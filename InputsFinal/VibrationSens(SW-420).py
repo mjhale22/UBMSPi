bc2ac70b57d3d74bd141b5dfdcacbc545ba7b6c4
Michael Hale <49446855+mjhale22@users.noreply.github.com>
2019-05-08 18:48:01 +0000

    Update VibrationSens(SW-420).py

--- InputsFinal/VibrationSens(SW-420).py
+++ InputsFinal/VibrationSens(SW-420).py
@@ -1,1 +1,21 @@
-Test
+# Import Raspberry Pi GPIO info as GPIO and Import time functionality
+import RPi.GPIO as GPIO
+import time
+
+# Set variable of gpio as pin 21
+gpio = 21
+GPIO.setmode(GPIO.BCM)
+GPIO.setup(gpio, GPIO.IN)
+
+def callback(gpio):
+        if GPIO.input(gpio):
+                print ("Vibration Detect!")
+        else:
+                print ("Vibration Detect!")
+
+GPIO.add_event_detect(gpio, GPIO.BOTH, bouncetime=300)  
+GPIO.add_event_callback(gpio, callback)  
+
+# Creates an infinite loop
+while True:
+        time.sleep(1)
