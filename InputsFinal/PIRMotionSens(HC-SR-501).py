ede3745fecd8bfef828badbcadba150ff9d36488
Michael Hale <49446855+mjhale22@users.noreply.github.com>
2019-05-09 11:16:57 -0400

    Update PIRMotionSens(HC-SR-501).py

--- InputsFinal/PIRMotionSens(HC-SR-501).py
+++ InputsFinal/PIRMotionSens(HC-SR-501).py
@@ -1,21 +1,41 @@
-# Wiring diagram and tutorial can be found at: https://tutorials-raspberrypi.com/connect-and-control-raspberry-pi-motion-detector-pir/
+# Wiring diagram and tutorial can be found at: https://www.piddlerintheroot.com/pir-sensor/
 
-import RPi.GPIO as GPIO
 import time
- 
-SENSOR_PIN = 23
- 
+import RPi.GPIO as GPIO
+
 GPIO.setmode(GPIO.BCM)
-GPIO.setup(SENSOR_PIN, GPIO.IN)
- 
-def my_callback(channel):
-    # Here, alternatively, an application / command etc. can be started.
-    print('There was a movement!')
- 
+#GPIO.setwarnings(False)
+
+led1 = 17
+led2 = 27
+pirPin = 26
+
+GPIO.setup(led1,GPIO.OUT)
+GPIO.setup(led2,GPIO.OUT)
+GPIO.setup(pirPin, GPIO.IN)
+
+def LIGHTS(pirPin):
+    """Turns LEDS On and Off"""
+    print("Motion Detected!")
+    print("Lights on")
+    GPIO.output(led1,GPIO.HIGH)
+    GPIO.output(led2,GPIO.HIGH)
+
+    time.sleep(2)
+
+    print("Light off")
+    GPIO.output(led1,GPIO.LOW)
+    GPIO.output(led2,GPIO.LOW)
+
+print("Motion Sensor Alarm (CTRL+C to exit)")
+time.sleep(.2)
+print("Ready")
+
+
 try:
-    GPIO.add_event_detect(SENSOR_PIN , GPIO.RISING, callback=my_callback)
-    while True:
-        time.sleep(100)
+    GPIO.add_event_detect(pirPin, GPIO.RISING, callback=LIGHTS)
+    while 1:
+        time.sleep(1)
 except KeyboardInterrupt:
-    print "Finish..."
-GPIO.cleanup()
+    print("Quit")
+    GPIO.cleanup()
