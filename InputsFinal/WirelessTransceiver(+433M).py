8329b6e1ff11e1f5ccf299eaee860f7135985bb6
Michael Hale <49446855+mjhale22@users.noreply.github.com>
2019-05-09 10:33:01 -0400

    Create WirelessTransceiver(+433M).py

---
+++ InputsFinal/WirelessTransceiver(+433M).py
@@ -0,0 +1,13 @@
+#Tutorial can be found at: https://tutorials-raspberrypi.com/let-raspberry-pis-communicate-with-each-other-per-433mhz-wireless-signals/
+
+#PRIOR TO RUNNING, TYPE FOLLOWNING INTO TERMINAL:
+#   sudo apt-get install git-core
+#   sudo apt-get update
+#   sudo apt-get upgrade
+#   git clone git://git.drogon.net/wiringPi && cd wiringPi &&./build
+#   git clone https://github.com/ninjablocks/433Utils.git
+#   cd 433Utils/RPi_utils
+#   make all
+
+#Code is unavailable for this item.  Still learning about how to use this module.
+#Please access tutorial link above and submit recommendations.
