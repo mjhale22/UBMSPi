b6634f02b0aee7c68e0dc140dbaac77a916440e2
Michael Hale <49446855+mjhale22@users.noreply.github.com>
2019-05-09 10:36:14 -0400

    Create SmokeSens(MQ-2).py

---
+++ InputsFinal/SmokeSens(MQ-2).py
@@ -0,0 +1,6 @@
+#Wiring diagram and tutorial can be found at: https://tutorials-raspberrypi.com/configure-and-read-out-the-raspberry-pi-gas-sensor-mq-x/
+
+#PRIOR TO RUNNING, TYPE FOLLOWNING INTO TERMINAL:
+#   git clone https://github.com/tutRPi/Raspberry-Pi-Gas-Sensor-MQ
+#   cd Raspberry-Pi-Gas-Sensor-MQ
+#   sudo python example.py
