1534668e84952a357fd0aa08bc94e59292663bc4
Michael Hale <49446855+mjhale22@users.noreply.github.com>
2019-05-09 11:23:58 -0400

    Update UltraSonicDistSens(HC-SR04).py

--- InputsFinal/UltraSonicDistSens(HC-SR04).py
+++ InputsFinal/UltraSonicDistSens(HC-SR04).py
@@ -1,40 +1,43 @@
-# Wiring diagram and walkthrough tutorial found at: https://pimylifeup.com/raspberry-pi-distance-sensor/
+# Wiring diagram and walkthrough tutorial found at: https://www.piddlerintheroot.com/hc-sr04/
 
-#!/usr/bin/python
 import RPi.GPIO as GPIO
 import time
+GPIO.setwarnings(False)
 
-try:
-      GPIO.setmode(GPIO.BOARD)
+def ping():
+	"""Get reading from HC-SR04"""
+	GPIO.setmode(GPIO.BCM)
+	 
+	TRIG = 23 
+	ECHO = 18
+	 
+	GPIO.setup(TRIG,GPIO.OUT)
+	GPIO.setup(ECHO,GPIO.IN)
+	 
+	GPIO.output(TRIG, False)
+	time.sleep(1)
+	 
+	GPIO.output(TRIG, True)
+	time.sleep(0.00001)
+	GPIO.output(TRIG, False)
+	 
+	while GPIO.input(ECHO)==0:
+	  pulse_start = time.time()
+	 
+	while GPIO.input(ECHO)==1:
+	  pulse_end = time.time()
+	 
+	pulse_duration = pulse_end - pulse_start
+	 
+	distance = pulse_duration * 17150
+	 
+	distance = round(distance, 2)
+	 
+	print "Distance:",distance,"cm"
+	 
+	GPIO.cleanup()
 
-      PIN_TRIGGER = 7
-      PIN_ECHO = 11
+print "Reading Distance \n"
 
-      GPIO.setup(PIN_TRIGGER, GPIO.OUT)
-      GPIO.setup(PIN_ECHO, GPIO.IN)
-
-      GPIO.output(PIN_TRIGGER, GPIO.LOW)
-
-      print "Waiting for sensor to settle"
-
-      time.sleep(2)
-
-      print "Calculating distance"
-
-      GPIO.output(PIN_TRIGGER, GPIO.HIGH)
-
-      time.sleep(0.00001)
-
-      GPIO.output(PIN_TRIGGER, GPIO.LOW)
-
-      while GPIO.input(PIN_ECHO)==0:
-            pulse_start_time = time.time()
-      while GPIO.input(PIN_ECHO)==1:
-            pulse_end_time = time.time()
-
-      pulse_duration = pulse_end_time - pulse_start_time
-      distance = round(pulse_duration * 17150, 2)
-      print "Distance:",distance,"cm"
-
-finally:
-      GPIO.cleanup()
+while True:
+	ping()
